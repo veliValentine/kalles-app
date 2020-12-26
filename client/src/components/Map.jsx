aa9852637211977d6f8cb67eeb8c401fdc27b859
veliValentine <nicolas.valentine@helsinki.fi>
2020-12-27 01:15:32 +0200

    playing with map component

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+import useCurrentLocation from '../hooks/useCurrentLocation';
 
 import Constants from 'expo-constants';
 
@@ -19,18 +21,32 @@ const styles = StyleSheet.create({
   }
 });
 
+//https://github.com/react-native-maps/react-native-maps
 const Map = () => {
+  const [location] = useCurrentLocation();
+  if (!location) {
+    return <Text>loading</Text>;
+  }
   return (
     <View>
       <View style={styles.header} />
       <MapView style={styles.map}
+        minZoomLevel={13}
         initialRegion={{
-          latitude: 60.78825,
-          longitude: 25.4324,
-          latitudeDelta: 10,
-          longitudeDelta: 10,
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
         }}
-      />
+      >
+        <Marker coordinate={location} pinColor="blue"/>{/* Own location */}
+        <Marker
+          coordinate={{
+            latitude: location.latitude+0.005,
+            longitude: location.longitude
+          }}
+        />
+      </MapView>
       <View style={styles.footer} />
     </View>
   );
