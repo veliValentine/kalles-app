8d187c8cf369710cdddc8a72a2e4e4e5b755f082
veliValentine <nicolas.valentine@helsinki.fi>
2021-01-15 08:14:07 +0200

    refactor Message component

--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -26,24 +26,24 @@ const Message = ({ message }) => {
   const history = useHistory();
   const { distance, username, id } = message;
 
-  if (distance > 0.1) {
-    return (
-      <View style={styles.messageContainer}>
-        <Text>Move closer to see the message</Text>
-        <Text>Distance: {distance} km</Text>
-      </View>
-    );
-  }
+  const tooFarAway = distance > 0.1;
+
   return (
-    <TouchableOpacity
-      onPress={() => history.push(`/message/${id}`)}
-    >
-      <View style={styles.messageContainer}>
-        <Text style={styles.messageText} >Click to see message</Text>
-        <Text>By: {username}</Text>
+    <View style={styles.messageContainer}>
+      <TouchableOpacity
+        onPress={() => history.push(`/message/${id}`)}
+        disabled={tooFarAway}
+      >
+        {tooFarAway
+          ? <Text>Move closer to see the message</Text>
+          : <View>
+            <Text style={styles.messageText} >Click to see message</Text>
+            <Text>By: {username}</Text>
+          </View>
+        }
         <Text>Distance: {distance} km</Text>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -51,7 +51,7 @@ const MessageList = ({ messages, }) => {
   return (
     <View style={styles.container}>
       <FlatList
-        data={filterByDistances(messages, 15).sort(sortByDistances)}
+        data={messages}
         keyExtractor={item => item.id}
         ItemSeparatorComponent={ItemSeparator}
         renderItem={({ item }) => <Message message={item} />}
