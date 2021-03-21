78a2c9d5582fc0266e82f7c930b765389b69416c
veliValentine <nicolas.valentine@helsinki.fi>
2021-03-21 18:07:45 +0200

    chore: mock data

--- server/index.js
+++ server/index.js
@@ -2,21 +2,36 @@ const express = require('express');
 const app = express();
 
 const messages = [
-  /*
-  messages to test client side
-  message: {
-    created
-    id
-    userId
+  {
+    id: '0',
+    text: 'Hello Helsinki',
     location: {
-      lat
-      long
-    }
-  }
-  */
+      latitude: 60.171712519065174,
+      longitude: 24.94059522394236
+    },
+    username: 'Helsinki',
+  },
+  {
+    id: '1',
+    text: 'Hello Kumpula',
+    location: {
+      latitude: 60.2058235648218,
+      longitude: 24.963834277842142
+    },
+    username: 'Kumpula',
+  },
+  {
+    id: '2',
+    text: 'Hello Viikki',
+    location: {
+      latitude: 60.22773733793554,
+      longitude: 25.014474383948446
+    },
+    username: 'Testuser 3',
+  },
 ];
 
-app.get('/health', (_req, res) => {
+app.get('/api/health', (_req, res) => {
   res.send('ok')
 });
 
@@ -28,4 +43,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
-
