d274b3459e2a82d7e5bf242ed0194e8e7fea0c58
veliValentine <nicolas.valentine@helsinki.fi>
2021-03-30 22:05:32 +0300

    feat: POST message checks invalid arguments

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,8 @@
 const messageRouter = require('express').Router();
 const { isTestEnvironment } = require('../utils/environment');
 const { currentTimeStamp } = require('../utils/time');
+const { biggestId } = require('../utils/utils');
+const { isString, isLocationObject } = require('../utils/validators');
 
 let MESSAGES_DATA = isTestEnvironment ? [] : [
   {
@@ -37,17 +39,37 @@ messageRouter.get('/', (_req, res) => {
 });
 
 messageRouter.post('/', (req, res) => {
-  const { body = {} } = req;
-  const id = Math.max(...MESSAGES_DATA.map((message) => message.id)) + 1;
+  const { body } = req;
+  if (!body) {
+    return res.status(400).json('Error: Missing request body');
+  }
+  const { message, username, location } = body;
+  if (!isString(message)) {
+    return res.status(400).json('Error: Invalid message');
+  }
+  if (!isString(username)) {
+    return res.status(400).json('Error: Invalid username');
+  }
+  if (!isLocationObject(location)) {
+    return res.status(400).json('Error: Invalid location');
+  }
+  const { latitude, longitude } = location;
+  const id = biggestId(MESSAGES_DATA.map((m) => m.id)) + 1;
   const now = currentTimeStamp();
-  const message = {
+  const newMessage = {
     id,
-    ...body,
+    message,
+    username,
+    location: {
+      latitude,
+      longitude,
+    },
     created: now,
+    expires: 24,
     likes: 0,
   };
-  MESSAGES_DATA = MESSAGES_DATA.concat(message);
-  res.status(201).json(message);
+  MESSAGES_DATA = MESSAGES_DATA.concat(newMessage);
+  return res.status(201).json(newMessage);
 });
 
 module.exports = messageRouter;
---
+++ server/utils/validators.js
@@ -0,0 +1,30 @@
+const isLocationObject = (locationObject) => {
+  if (!locationObject) {
+    return false;
+  }
+  const { latitude, longitude } = locationObject;
+  if (!isNumber(latitude) || !isNumber(longitude)) {
+    return false;
+  }
+  return true;
+};
+
+const isNumber = (input = null) => {
+  if (input === null || typeof input !== 'number') {
+    return false;
+  }
+  return true;
+};
+
+const isString = (input) => {
+  if (!input || typeof input !== 'string') {
+    return false;
+  }
+  return true;
+};
+
+module.exports = {
+  isString,
+  isNumber,
+  isLocationObject,
+};
