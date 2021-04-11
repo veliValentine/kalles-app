55dfe53280840460883a975fa97ef68cdbd2ec20
veliValentine <nicolas.valentine@helsinki.fi>
2021-04-11 19:06:59 +0300

    fix: month index started from 0

--- server/utils/time.js
+++ server/utils/time.js
@@ -1,7 +1,7 @@
 const currentTimeStamp = () => {
   const date = new Date();
   const year = date.getUTCFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getUTCDate();
   const hours = date.getUTCHours();
   const minutes = date.getMinutes();
