a2f4012f67f1d6dac47196a87c6540f861821575
veliValentine <nicolas.valentine@helsinki.fi>
2021-04-11 17:07:42 +0300

    refactor: message tests

--- server/tests/messageController.test.js
+++ server/tests/messageController.test.js
@@ -24,8 +24,7 @@ describe('message', () => {
     test('returns array', async () => {
       const { body: messages } = await api
         .get(MESSAGES_ENDPOINT);
-      expect(Array.isArray(messages)).toBe(true);
-      expect(messages.length).toBe(0);
+      expect(messages).toEqual([]);
     });
   });
 
@@ -38,125 +37,130 @@ describe('message', () => {
         longitude: 0.00,
       },
     };
-    test('new message response 201', async () => {
-      await api
-        .post(MESSAGES_ENDPOINT)
-        .send(validMessage)
-        .set('Accept', 'application/json')
-        .expect(201);
+
+    describe('Valid messages', () => {
+      test('response 201', async () => {
+        await api
+          .post(MESSAGES_ENDPOINT)
+          .send(validMessage)
+          .set('Accept', 'application/json')
+          .expect(201);
+      });
+
+      test('valid message added', async () => {
+        const messagesBefore = await getMessages(api);
+        const created = currentTimeStamp();
+        const { body: addedMessage } = await api
+          .post(MESSAGES_ENDPOINT)
+          .send(validMessage)
+          .set('Accept', 'application/json')
+          .expect(201);
+        expect(addedMessage).toEqual(
+          {
+            id: addedMessage.id,
+            ...validMessage,
+            created,
+            expires: 24,
+            likes: 0,
+            distance: 0,
+          },
+        );
+        const messagesAfter = await getMessages(api);
+        expect(messagesAfter).toHaveLength(messagesBefore.length + 1);
+      });
     });
 
-    test('valid message added', async () => {
-      const messagesBefore = await getMessages(api);
-      const created = currentTimeStamp();
-      const { body: addedMessage } = await api
-        .post(MESSAGES_ENDPOINT)
-        .send(validMessage)
-        .set('Accept', 'application/json')
-        .expect(201);
-      expect(addedMessage).toEqual(
-        {
-          id: addedMessage.id,
-          ...validMessage,
-          created,
-          expires: 24,
-          likes: 0,
-          distance: 0,
-        },
-      );
-      const messagesAfter = await getMessages(api);
-      expect(messagesAfter.length).toBe(messagesBefore.length + 1);
-    });
+    describe('invalid messages', () => {
+      test('missing username response 400', async () => {
+        const messagesBefore = await getMessages(api);
+        const missingUsername = {
+          message: 'testMessage',
+          location: {
+            latitude: 0.00,
+            longitude: 0.00,
+          },
+        };
+        const { body: errorMessage } = await api
+          .post(MESSAGES_ENDPOINT)
+          .send(missingUsername)
+          .set('Accept', 'application/json')
+          .expect(400);
 
-    test('missing username response 400', async () => {
-      const messagesBefore = await getMessages(api);
-      const missingUsername = {
-        message: 'testMessage',
-        location: {
-          latitude: 0.00,
-          longitude: 0.00,
-        },
-      };
-      await api
-        .post(MESSAGES_ENDPOINT)
-        .send(missingUsername)
-        .set('Accept', 'application/json')
-        .expect(400);
+        const messagesAfter = await getMessages(api);
+        expect(messagesBefore.length).toBe(messagesAfter.length);
+      });
 
-      const messagesAfter = await getMessages(api);
-      expect(messagesBefore.length).toBe(messagesAfter.length);
-    });
+      test('missing message response 400', async () => {
+        const messagesBefore = await getMessages(api);
+        const missingMessage = {
+          username: 'testUsername',
+          location: {
+            latitude: 0.00,
+            longitude: 0.00,
+          },
+        };
+        await api
+          .post(MESSAGES_ENDPOINT)
+          .send(missingMessage)
+          .set('Accept', 'application/json')
+          .expect(400);
 
-    test('missing message response 400', async () => {
-      const messagesBefore = await getMessages(api);
-      const missingMessage = {
-        username: 'testUsername',
-        location: {
-          latitude: 0.00,
-          longitude: 0.00,
-        },
-      };
-      await api
-        .post(MESSAGES_ENDPOINT)
-        .send(missingMessage)
-        .set('Accept', 'application/json')
-        .expect(400);
+        const messagesAfter = await getMessages(api);
+        expect(messagesBefore.length).toBe(messagesAfter.length);
+      });
 
-      const messagesAfter = await getMessages(api);
-      expect(messagesBefore.length).toBe(messagesAfter.length);
-    });
+      test('invalid location response 400', async () => {
+        const messagesBefore = await getMessages(api);
+        const missinglocation = {
+          username: 'testUsername',
+          message: 'testMessage',
+        };
+        await api
+          .post(MESSAGES_ENDPOINT)
+          .send(missinglocation)
+          .set('Accept', 'application/json')
+          .expect(400);
 
-    test('invalid location response 400', async () => {
-      const messagesBefore = await getMessages(api);
-      const missinglocation = {
-        username: 'testUsername',
-        message: 'testMessage',
-      };
-      await api
-        .post(MESSAGES_ENDPOINT)
-        .send(missinglocation)
-        .set('Accept', 'application/json')
-        .expect(400);
+        const emptyLocation = {
+          username: 'testUsername',
+          message: 'testMessage',
+          location: {},
+        };
+        await api
+          .post(MESSAGES_ENDPOINT)
+          .send(emptyLocation)
+          .set('Accept', 'application/json')
+          .expect(400);
 
-      const emptyLocation = {
-        username: 'testUsername',
-        message: 'testMessage',
-        location: {},
-      };
-      await api
-        .post(MESSAGES_ENDPOINT)
-        .send(emptyLocation)
-        .set('Accept', 'application/json')
-        .expect(400);
+        const missingLatitude = {
+          username: 'testUsername',
+          message: 'testMessage',
+          location: {
+            longitude: 0.0,
+          },
+        };
+        await api
+          .post(MESSAGES_ENDPOINT)
+          .send(missingLatitude)
+          .set('Accept', 'application/json')
+          .expect(400);
 
-      const missingLatitude = {
-        username: 'testUsername',
-        message: 'testMessage',
-        location: {
-          longitude: 0.0,
-        },
-      };
-      await api
-        .post(MESSAGES_ENDPOINT)
-        .send(missingLatitude)
-        .set('Accept', 'application/json')
-        .expect(400);
+        const missingLongitude = {
+          username: 'testUsername',
+          message: 'testMessage',
+          location: {
+            latitude: 0.0,
+          },
+        };
+        await api
+          .post(MESSAGES_ENDPOINT)
+          .send(missingLongitude)
+          .set('Accept', 'application/json')
+          .expect(400);
 
-      const missingLongitude = {
-        username: 'testUsername',
-        message: 'testMessage',
-        location: {
-          latitude: 0.0,
-        },
-      };
-      await api
-        .post(MESSAGES_ENDPOINT)
-        .send(missingLongitude)
-        .set('Accept', 'application/json')
-        .expect(400);
-
-      const messagesAfter = await getMessages(api);
-      expect(messagesBefore.length).toBe(messagesAfter.length);
+        const messagesAfter = await getMessages(api);
+        expect(messagesBefore.length).toBe(messagesAfter.length);
+      });
     });
 
     // TODO check missing attributes in request body => status 400
