30ffa5569bf704f51dbae2cce1d83d6733df3cd8
veliValentine <nicolas.valentine@helsinki.fi>
2021-04-24 12:59:54 +0300

    refactor: utils folder

---
+++ client/src/utils/URL.js
@@ -0,0 +1,6 @@
+const HEROKU_URL_BASE = 'https://kalle-studio.herokuapp.com/api/v1';
+const LOCAL_URL_BASE = 'http://localhost:3001/api/v1';
+
+// eslint-disable-next-line no-undef
+export const SERVER_URL_BASE = __DEV__ ? LOCAL_URL_BASE : HEROKU_URL_BASE;
+
