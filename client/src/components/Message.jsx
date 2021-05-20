c24c3d57fc3b82b3aede5598165b1d3294b13e8e
veliValentine <nicolas.valentine@helsinki.fi>
2021-05-20 18:37:13 +0300

    refactor: message into logical smaller components

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -4,6 +4,44 @@ import { useParams, useHistory } from 'react-router-native';
 
 import LoadingScreen from './LoadingScreen';
 
+const Message = ({ messages }) => {
+  const { id } = useParams();
+  if (!messages) {
+    return <LoadingScreen message={'getting messages'} />;
+  }
+  const message = messages.find((item) => item.id === id);
+  if (!message) {
+    return <MessageNotFound id={id} />;
+  }
+  return <MessageFound message={message} />;
+};
+
+const MessageNotFound = ({ id }) => {
+  console.log(`Error getting message --- Message.jsx --- Message not found. ID: ${id}`);
+  const history = useHistory();
+  const handlePress = () => history.push('/');
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Message not found!</Text>
+        <Text style={{ color: 'blue' }}>Go to main menu</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const MessageFound = ({ message }) => {
+  const { text, username } = message;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.username}>{username}</Text>
+      <Text style={styles.text}>Created @ ...</Text>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
@@ -18,43 +56,5 @@ const styles = StyleSheet.create({
     padding: 5,
   },
 });
-
-const Message = ({ messages }) => {
-  const { id } = useParams();
-  const history = useHistory();
-
-  if (!messages) {
-    return <LoadingScreen />;
-  }
-
-  const message = messages.find((item) => item.id === id);
-
-  if (!message) {
-    console.log(`Error getting message --- Message.jsx --- Message not found. ID: ${id}`);
-
-    const handlePress = () => {
-      history.push('/');
-    };
-
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={handlePress}>
-          <Text>Message not found!</Text>
-          <Text style={{ color: 'blue' }}>Go to main menu</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
-  const { text, username } = message;
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.username}>{username}</Text>
-      <Text style={styles.text}>Created @ ...</Text>
-      <Text style={styles.text}>{text}</Text>
-    </View>
-  );
-};
 
 export default Message;
