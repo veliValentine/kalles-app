843cb06ddd0b1301cf4178e3c52a5933274610c0
veliValentine <nicolas.valentine@helsinki.fi>
2021-05-24 20:10:19 +0300

    refactor: pure component and logic component

--- client/src/components/MapPage/Marker.jsx
+++ client/src/components/MapPage/Marker.jsx
@@ -1,30 +1,53 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 
-import { isReadable } from '../../utils';
-import { YELLOW_MESSAGE_THRESHOLD } from '../../utils';
-
 import { Marker as NativeMarker } from 'react-native-maps';
+import { Alert } from 'react-native';
+
+import { isReadable, YELLOW_MESSAGE_THRESHOLD, READABLE_TRESHOLD } from '../../utils';
 
 const Marker = ({ message }) => {
   const history = useHistory();
-  const { coordinate, id, username, distance } = message;
+  const { distance, id, username, location } = message;
   const isClose = isReadable(distance);
+  const pinColor = isClose ? 'green' : distance < YELLOW_MESSAGE_THRESHOLD ? 'yellow' : 'red';
+  const title = isClose ? 'Click to read' : `Distance to message ${distance}km`;
+
   const redirectToMessageView = () => {
     if (isClose) {
       history.push(`/message/${id}`);
+    } else {
+      messageNotReadable();
     }
   };
+  const messageNotReadable = () => {
+    const alertTitle = 'You are too far away from the message';
+    const alertMessage = [
+      `Message is ${distance}km from your location.`,
+      `You need to be within ${READABLE_TRESHOLD}km to read the message.`
+    ];
+    Alert.alert(alertTitle, alertMessage.join('\n\n'));
+  };
   return (
-    <NativeMarker
-      coordinate={coordinate}
-      pinColor={isClose ? 'green' : distance < YELLOW_MESSAGE_THRESHOLD ? 'yellow' : 'red'}
+    <ViewMarker
       key={id}
-      title={isClose ? 'Click to read' : `Distance to message ${distance}km`}
+      location={location}
+      pinColor={pinColor}
+      title={title}
       description={`By ${username}`}
       onCalloutPress={redirectToMessageView}
     />
   );
 };
+
+export const ViewMarker = ({ location, pinColor, title, description, onCalloutPress }) => (
+  <NativeMarker
+    coordinate={location}
+    pinColor={pinColor}
+    title={title}
+    description={description}
+    onCalloutPress={onCalloutPress}
+  />
+);
 
 export default Marker;
