a398beadbeff9af906e713b0746355a52f64d0f6
veliValentine <nicolas.valentine@helsinki.fi>
2021-05-24 19:36:01 +0300

    refactor: map button

--- client/src/components/MapPage/MapButtons.jsx
+++ client/src/components/MapPage/MapButtons.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export const ReloadButton = ({ onPress }) => (
-  <TouchableOpacity onPress={onPress} style={styles.reloadContainer}>
-    <View style={styles.reloadButton}>
-      <Text style={styles.text}>Reload messages</Text>
+export const MapButton = ({ onPress, containerStyle, buttonStyle, textStyle, buttonText }) => (
+  <TouchableOpacity onPress={onPress} style={{ ...styles.container, ...containerStyle }}>
+    <View style={{ ...styles.button, ...buttonStyle }}>
+      <Text style={{ ...styles.text, ...textStyle }}>{buttonText}</Text>
     </View>
   </TouchableOpacity >
 );
 
+export const ReloadButton = ({ onPress }) => (
+  <MapButton
+    onPress={onPress}
+    containerStyle={styles.reloadContainer}
+    buttonText={'Reload messages'}
+  />
+);
+
 export const UserLocationButton = ({ onPress }) => (
-  <TouchableOpacity onPress={onPress} style={styles.ownLocationContainer}>
-    <View style={styles.ownLocationButton}>
-      <Text style={styles.text}>My location</Text>
-    </View>
-  </TouchableOpacity >
+  <MapButton
+    onPress={onPress}
+    containerStyle={styles.userLocationContainer}
+    buttonText={'My location'}
+  />
 );
 
 const styles = StyleSheet.create({
   reloadContainer: {
-    position: 'absolute',
     alignSelf: 'center',
+  },
+  userLocationContainer: {
+    alignSelf: 'flex-end'
+  },
+  container: {
+    position: 'absolute',
     padding: 20,
   },
-  reloadButton: {
+  button: {
     backgroundColor: 'white',
     borderRadius: 50,
     padding: 5,
@@ -33,18 +46,5 @@ const styles = StyleSheet.create({
   },
   text: {
     padding: 1
-  },
-  ownLocationContainer: {
-    position: 'absolute',
-    alignSelf: 'flex-end',
-    padding: 20,
-  },
-  ownLocationButton: {
-    backgroundColor: 'white',
-    borderRadius: 50,
-    padding: 5,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderWidth: 1,
   },
 });
