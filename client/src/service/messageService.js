e514787f3cb425a7c8f1c40b39082972af0a0ac2
veliValentine <nicolas.valentine@helsinki.fi>
2021-06-07 20:06:14 +0300

    feat: check GET message response status 200

--- client/src/service/messageService.js
+++ client/src/service/messageService.js
@@ -4,13 +4,18 @@ import { SERVER_URL_BASE } from '../utils/URL';
 
 const MESSAGE_URL = `${SERVER_URL_BASE}/messages`;
 
-const STATUS_CREATED = 201;
+const HTTP_OK = 200;
+const HTTP_CREATED = 201;
 
 export const fetchMessages = async (location) => {
   try {
     const query = coordinateQuery(location);
     const URL = `${MESSAGE_URL}?${query}`;
     const response = await fetch(URL);
+    const responseStatus = response.status;
+    if (responseStatus !== HTTP_OK) {
+      throw new Error(`Server responded with status: ${responseStatus}`);
+    }
     const messages = await parseServerResponse(response);
     return messages;
   } catch (e) {
@@ -41,8 +46,9 @@ export const addMessage = async (message) => {
     body: JSON.stringify(message)
   });
   const responseJSON = await response.json();
-  if (response.status !== STATUS_CREATED) {
-    throw new Error(responseJSON);
+  const responseStatus = response.status;
+  if (responseStatus !== HTTP_CREATED) {
+    throw new Error(`Server responded with status: ${responseStatus}`, responseJSON);
   }
   return responseJSON;
 };
