38731b625d8c3e05a7ca4cae3ce105dd8cf9f9b9
veliValentine <nicolas.valentine@helsinki.fi>
2021-06-08 20:10:36 +0300

    feat: removed storage initation

--- client/App.js
+++ client/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Main from './src/Main';
 
 import { NativeRouter } from 'react-router-native';
@@ -7,22 +7,12 @@ import StorageContext from './src/contexts/StorageContext';
 
 const storageService = new StorageService();
 
-const App = () => {
-  useEffect(() => {
-    console.log('Clear storage');
-    const clearStorage = async () => {
-      await storageService.clearMessages();
-    };
-    clearStorage();
-  }, []);
-
-  return (
-    <NativeRouter>
-      <StorageContext.Provider value={storageService} >
-        <Main />
-      </StorageContext.Provider>
-    </NativeRouter>
-  );
-};
+const App = () => (
+  <NativeRouter>
+    <StorageContext.Provider value={storageService} >
+      <Main />
+    </StorageContext.Provider>
+  </NativeRouter>
+);
 
 export default App;
