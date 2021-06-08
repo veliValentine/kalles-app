058676022c3fe11ee8b22f7d8d958cfb90ae141f
veliValentine <nicolas.valentine@helsinki.fi>
2021-06-08 20:07:45 +0300

    feat: return error message

--- client/src/utils/errors.js
+++ client/src/utils/errors.js
@@ -2,7 +2,7 @@ export const handleError = (e, message) => {
   console.log(message);
   if (e instanceof Error) {
     console.log(e.message);
-    return;
+    return e.message;
   }
   console.log('Error thrown');
   throw e;
