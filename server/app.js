aab5649c5dfaf3fdd7734d0fefd06b004dcdd27f
veliValentine <nicolas.valentine@helsinki.fi>
2021-06-13 12:59:04 +0300

    feat: morgan middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const messageRouter = require('./controllers/messageController');
 const utilController = require('./controllers/utilController');
 
 const app = express();
 
 app.use(express.json());
+morgan.token('request-body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :response-time ms - :request-body'));
 
 const V1_ROUTE = '/api/v1';
 
