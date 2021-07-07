8cc8cf01eed923f80e6621c38d408247f6a4eec6
veliValentine <nicolas.valentine@helsinki.fi>
2021-07-07 20:20:53 +0300

    feat: dev db url

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,4 +1,4 @@
-const { isTestEnvironment } = require('./environment');
+const { isTestEnvironment, isDevelopmentEnvironment } = require('./environment');
 
 require('dotenv').config();
 
@@ -7,6 +7,9 @@ const { PORT } = process.env;
 let MONGODB_URI = process.env.MONGO_MESSAGES_URI;
 if (isTestEnvironment) {
   MONGODB_URI = process.env.MONGO_MESSAGES_TEST_URI;
+}
+if (isDevelopmentEnvironment) {
+  MONGODB_URI = process.env.MONGO_MESSAGES_DEV_URI;
 }
 
 module.exports = {
