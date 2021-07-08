362e5fb1068c5ed436e8a86311f7b5ad396a455b
veliValentine <nicolas.valentine@helsinki.fi>
2021-07-08 20:18:57 +0300

    refactor: typo

--- server/controllers/controllerHelpers.js
+++ server/controllers/controllerHelpers.js
@@ -1,10 +1,10 @@
 const handleError400 = (res, message) => handleErrorStatus(400, res, message);
 
-const handeError404 = (res, message) => handleErrorStatus(404, res, message);
+const handleError404 = (res, message) => handleErrorStatus(404, res, message);
 
 const handleErrorStatus = (status, res, message) => res.status(status).json({ error: message });
 
 module.exports = {
   handleError400,
-  handeError404,
+  handleError404,
 };
