3dd6cd0f02b071fef5f7caa4281ee4b5be326275
unknown <nikkevanukas1995@gmail.com>
2021-08-21 13:18:14 +0300

    feat: range error to location properties

--- server/models/message.js
+++ server/models/message.js
@@ -16,10 +16,14 @@ const messageSchema = new mongoose.Schema({
     latitude: {
       type: Number,
       required: true,
+      min: -90,
+      max: 90,
     },
     longitude: {
       type: Number,
       required: true,
+      min: -180,
+      max: 180,
     },
   },
   createDay: {
