d6da0478aed3cfa9b8fa1acbd6e022986c3c1509
unknown <nikkevanukas1995@gmail.com>
2021-08-31 18:57:59 +0300

    refactor: use storage function

--- client/App.js
+++ client/App.js
@@ -2,16 +2,10 @@ import React from 'react';
 import Main from './src/Main';
 
 import { NativeRouter } from 'react-router-native';
-import StorageContext from './src/contexts/StorageContext';
-import StorageService from './src/service/storage/storageService';
-
-const storageService = new StorageService();
 
 const App = () => (
   <NativeRouter>
-    <StorageContext.Provider value={storageService} >
       <Main />
-    </StorageContext.Provider>
   </NativeRouter>
 );
 
