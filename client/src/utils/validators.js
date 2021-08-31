3d1704afa094dcd2ad051c3e211e60944d6d975b
unknown <nikkevanukas1995@gmail.com>
2021-08-31 19:01:16 +0300

    feat: reusable user validator

---
+++ client/src/utils/validators.js
@@ -0,0 +1,8 @@
+const validateUser = (user) => {
+    if(!user) throw new Error('No user given');
+    if(!user.username) throw new Error('No username given');
+};
+
+export default {
+    validateUser,
+};
