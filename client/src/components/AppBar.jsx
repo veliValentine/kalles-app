fff0e4cc8f8bed63da6b40e941ae9041bb820c37
unknown <nikkevanukas1995@gmail.com>
2021-09-05 19:04:48 +0300

    feat: userpage

--- client/src/components/AppBar.jsx
+++ client/src/components/AppBar.jsx
@@ -4,35 +4,40 @@ import { Link } from 'react-router-native';
 
 import Constants from 'expo-constants';
 
-const AppBar = () => (
-  <View style={styles.container}>
-    <ScrollView horizontal>
-      <AppBarTab to="/map">Map</AppBarTab>
-      <AppBarTab to="/messages">Messages</AppBarTab>
-      <AppBarTab to="/newMessage">New message</AppBarTab>
-      <AppBarTab>{Constants.manifest.version}</AppBarTab>
-    </ScrollView>
-  </View>
+const AppBar = ({ user }) => (
+	<View style={styles.container}>
+		<ScrollView horizontal>
+			<AppBarTab to="/map">Map</AppBarTab>
+			<AppBarTab to="/messages">Messages</AppBarTab>
+			<AppBarTab to="/newMessage">New message</AppBarTab>
+			{user ?
+				<AppBarTab to="/userinfo">{user.username}</AppBarTab> :
+				null
+			}
+			<AppBarTab>{Constants.manifest.version}</AppBarTab>
+		</ScrollView>
+	</View>
 );
 
 const AppBarTab = ({ to, children }) => (
-  <Link to={to}>
-    <Text style={styles.text}>{children}</Text>
-  </Link>
+	<Link to={to}>
+		<Text style={styles.text}>{children}</Text>
+	</Link>
 );
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
-    flexDirection: 'row',
-    padding: 10,
-  },
-  text: {
-    padding: 10,
-    color: 'white',
-    marginRight: 20,
-    fontWeight: 'bold',
-  },
+	container: {
+		backgroundColor: 'black',
+		flexDirection: 'row',
+		padding: 10,
+		height: 55
+	},
+	text: {
+		padding: 10,
+		color: 'white',
+		marginRight: 20,
+		fontWeight: 'bold',
+	},
 });
 
 export default AppBar;
