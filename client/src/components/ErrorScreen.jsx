6ac4ad077a1fee2fb42aeb365ae2365ab4ce8c24
unknown <nikkevanukas1995@gmail.com>
2021-10-04 19:34:08 +0300

    feat: error screen

---
+++ client/src/components/ErrorScreen.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ErrorScreen = ({ errorMessage }) => {
+	if (!errorMessage) return null;
+	return <ErrorView errorMessage={errorMessage} />;
+};
+
+const ErrorView = ({ errorMessage = 'An unexpected error occured' }) => (
+	<View style={styles.container}>
+		<Text style={styles.text}>{errorMessage}</Text>
+	</View>
+);
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 10,
+		backgroundColor: 'red',
+	},
+	text: {
+		color: 'black',
+		fontWeight: 'bold',
+		textAlign: 'center'
+	}
+});
+
+export default ErrorScreen;
