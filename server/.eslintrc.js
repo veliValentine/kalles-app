ef0ca4381b35954ee528a540487d40f0332d8eb4
unknown <nikkevanukas1995@gmail.com>
2021-12-01 18:55:16 +0200

    chore: eslint rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -6,18 +6,19 @@ module.exports = {
 		jest: true,
 	},
 	extends: [
-		'eslint:recommended',
-		'airbnb-base',
+		"eslint:recommended",
+		"airbnb-base",
 	],
 	parserOptions: {
 		ecmaVersion: 12,
 	},
 	rules: {
-		'linebreak-style': 'off',
-		'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-		'no-use-before-define': ['error', { variables: false }],
-		'prefer-destructuring': ['error', { VariableDeclarator: { object: true } }],
-		indent: ['error', 'tab'],
-		allowIndentationTabs: true,
+		"linebreak-style": "off",
+		"no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+		"no-use-before-define": ["error", { variables: false }],
+		"prefer-destructuring": ["error", { VariableDeclarator: { object: true } }],
+		indent: ["error", "tab"],
+		"no-tabs": ["error", { allowIndentationTabs: true }],
+		quotes: ["error", "double"],
 	},
 };
