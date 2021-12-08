ec6e3e6b37ce55bc9177566dc73c450dc60acbae
unknown <nikkevanukas1995@gmail.com>
2021-12-08 21:09:48 +0200

    feat: user model

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new mongoose.Schema({
+	id: {
+		type: String,
+		required: true,
+		unique: true,
+		index: true,
+		minlength: 1,
+	},
+	username: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: [3, "Username too short. {VALUE} is too short"],
+		maxlength: [30, "Username too long. {VALUE} is too long."],
+		match: [/[a-zA-Z0-9]*/, "Username must contain characters and numbers. {VALUE} is not supported."],
+	},
+	messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+	liked: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+});
+
+userSchema.set("toJSON", {
+	transform: (_document, returnedObject) => {
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
+
+module.exports = mongoose.model("User", userSchema);
