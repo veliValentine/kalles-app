b2764a5e64cd8e4239bd558af91bfd4cf8240a82
unknown <nikkevanukas1995@gmail.com>
2021-12-11 19:15:27 +0200

    feat: error handling

--- server/src/main/middleware/authorizationTokenMiddleware.js
+++ server/src/main/middleware/authorizationTokenMiddleware.js
@@ -5,12 +5,25 @@ const authorizationTokenMiddleware = async (req, _res, next) => {
 	if (isBearerToken(authorization)) {
 		const token = authorization.substr(7);
 		req.token = token;
-		const uid = await firebaseService.getUserUid(token);
-		req.id = uid;
+		try {
+			const uid = await firebaseService.getUserUid(token);
+			req.id = uid;
+		} catch (error) {
+			handleFirebaseError(error, req);
+		}
 	}
 	next();
 };
 
 const isBearerToken = (token) => token && token.startsWith("Bearer ");
+
+const handleFirebaseError = (error, req) => {
+	if (error instanceof Error) {
+		const { message } = error;
+		if (message.includes("Firebase ID token has expired.")) {
+			req.token = null;
+		}
+	}
+};
 
 module.exports = authorizationTokenMiddleware;
