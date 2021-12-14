d0b3241d8f7523782a67532487b1ef0b51f5cd5e
unknown <nikkevanukas1995@gmail.com>
2021-12-14 21:11:49 +0200

    feat: init firebase app

--- server/src/main/service/firebaseService.js
+++ server/src/main/service/firebaseService.js
@@ -1,4 +1,8 @@
 const admin = require("firebase-admin");
+const firebaseAuth = require("firebase/auth");
+const firebaseApp = require("firebase/app");
+const logger = require("../utils/logger");
+const { isTestEnvironment } = require("../utils/environment");
 
 const initFirebase = () => {
 	const cert = {
@@ -9,6 +13,13 @@ const initFirebase = () => {
 	admin.initializeApp({
 		credential: admin.credential.cert(cert),
 	});
+
+	if (isTestEnvironment) {
+		const firebaseConfig = {
+			apiKey: process.env.FIREBASE_APP_API_KEY,
+		};
+		firebaseApp.initializeApp(firebaseConfig);
+	}
 };
 
 const decodeToken = (token) => (
@@ -22,7 +33,28 @@ const getUserUid = async (jwtToken) => {
 	return uid;
 };
 
+const getAccessToken = async (email, password) => {
+	if (!isTestEnvironment) throw new Error("Function unavailable");
+	const { accessToken } = await login(email, password);
+	return accessToken;
+};
+
+const login = async (email, password) => {
+	if (!isTestEnvironment) throw new Error("Function unavailable");
+	try {
+		const { user } = await firebaseAuth.signInWithEmailAndPassword(
+			firebaseAuth.getAuth(),
+			email,
+			password,
+		);
+		return user;
+	} catch (error) {
+		return logger.errorConsole(error);
+	}
+};
+
 module.exports = {
 	initFirebase,
 	getUserUid,
+	getAccessToken,
 };
