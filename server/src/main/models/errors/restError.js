1902d7d61db23086557abb183652dd9cf4f89439
unknown <nikkevanukas1995@gmail.com>
2022-01-06 13:17:05 +0200

    feat: clean up error message

--- server/src/main/models/errors/restError.js
+++ server/src/main/models/errors/restError.js
@@ -1,6 +1,6 @@
 class RestError extends Error {
 	constructor(status = 500, message = "Server error") {
-		super(message);
+		super(message.trim());
 		this.name = "RestError";
 		this.status = status;
 	}
