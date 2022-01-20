fbbac56ff20b82e4a5526c33cb6bbd8eb5288df6
unknown <nikkevanukas1995@gmail.com>
2022-01-20 19:42:37 +0200

    feat: scroll view

--- client/src/components/authentication/Register.jsx
+++ client/src/components/authentication/Register.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Button, ScrollView, Text, View } from "react-native";
 import { useState } from "react/cjs/react.development";
 import useError from "../../hooks/useError";
-import Error from "../common/Error";
+import ErrorComponent from "../common/Error";
 import { Input, styles } from "./commonAuth";
 
 const Register = ({ register, error }) => {
@@ -14,79 +14,87 @@ const Register = ({ register, error }) => {
 	const [validationError, setValidationError] = useError();
 
 	const handleRegister = () => {
-		validateUsername(username, setValidationError);
-		validateEmail(email, setValidationError);
-		validateEmail(emailAgain, setValidationError);
-		validatePassword(password, setValidationError);
-		validatePassword(passwordAgain, setValidationError);
-		if (email !== emailAgain) {
-			return setValidationError("Email mismatch");
+		try {
+			validateUsername(username);
+			validateEmail(email);
+			validateEmail(emailAgain);
+			validatePassword(password);
+			validatePassword(passwordAgain);
+			if (email !== emailAgain) {
+				throw new Error("Email mismatch");
+			}
+			if (password !== passwordAgain) {
+				throw new Error("Password mismatch");
+			}
+			register(username, email, password);
+		} catch (error) {
+			if (error instanceof Error) {
+				setValidationError(error.message);
+			}
 		}
-		if (password !== passwordAgain) {
-			return setValidationError("Password mismatch");
-		}
-		register(username, email, password);
 	};
 	return (
 		<View style={styles.container}>
-			<Text style={styles.header}>Register</Text>
-			<Input text="Enter your username: "
-				placeholder="Username"
-				value={username}
-				handleTextChange={setUsername}
-			/>
-			<Input text="Enter your email: "
-				placeholder="Email"
-				value={email}
-				handleTextChange={setEmail}
-			/>
-			<Input text="Enter your email again: "
-				placeholder="Email again"
-				value={emailAgain}
-				handleTextChange={setEmailAgain}
-			/>
-			<Input text="Enter your password: "
-				placeholder="Password"
-				value={password}
-				handleTextChange={setPassword}
-				isPassword={true}
-			/>
-			<Input text="Enter your password again: "
-				placeholder="Password again"
-				value={passwordAgain}
-				handleTextChange={setPasswordAgain}
-				isPassword={true}
-			/>
-			<Error errorMessage={error || validationError} />
-			<Button
-				onPress={handleRegister}
-				title="register"
-			/>
+			<ScrollView>
+				<Text style={styles.header}>Register</Text>
+				<Input text="Enter your username: "
+					placeholder="Username"
+					value={username}
+					handleTextChange={setUsername}
+				/>
+				<Input text="Enter your email: "
+					placeholder="Email"
+					value={email}
+					handleTextChange={setEmail}
+				/>
+				<Input text="Enter your email again: "
+					placeholder="Email again"
+					value={emailAgain}
+					handleTextChange={setEmailAgain}
+				/>
+				<Input text="Enter your password: "
+					placeholder="Password"
+					value={password}
+					handleTextChange={setPassword}
+					isPassword={true}
+				/>
+				<Input text="Enter your password again: "
+					placeholder="Password again"
+					value={passwordAgain}
+					handleTextChange={setPasswordAgain}
+					isPassword={true}
+				/>
+				<ErrorComponent errorMessage={error || validationError} />
+				<Button
+					onPress={handleRegister}
+					title="register"
+				/>
+			</ScrollView>
 		</View>
 	);
 };
 
-const validateUsername = (username, setError) => {
+const validateUsername = (username) => {
 	if (!username) {
-		return setError("Missing username");
+		throw new Error("Missing username");
 	}
 	if (username.length < 3) {
-		return setError("Username too short");
+		throw new Error("Username too short");
 	}
 	if (username.length > 30) {
-		return setError("Username too long");
+		throw new Error("Username too long");
 	}
 };
 
-const validateEmail = (email, setError) => {
+const validateEmail = (email) => {
 	if (!email) {
-		return setError("Missing email");
+		throw new Error("Missing email");
 	}
 };
 
-const validatePassword = (password, setError) => {
+const validatePassword = (password) => {
 	if (!password) {
-		return setError("Missing password");
+		throw new Error("Missing password");
 	}
 };
 
