17476b9da4a0ae91029cda2919fc648ab4cf0f4d
unknown <nikkevanukas1995@gmail.com>
2022-01-20 19:18:09 +0200

    feat: user appbar

--- client/src/components/AppBar.jsx
+++ client/src/components/AppBar.jsx
@@ -4,20 +4,27 @@ import { Link } from "react-router-native";
 
 import Constants from "expo-constants";
 
-const AppBar = ({ user }) => (
-	<View style={styles.container}>
-		<ScrollView horizontal>
-			<AppBarTab to="/map">Map</AppBarTab>
-			<AppBarTab to="/messages">Messages</AppBarTab>
-			<AppBarTab to="/newMessage">New message</AppBarTab>
-			{user ?
-				<AppBarTab to="/userinfo">{user.username}</AppBarTab> :
-				null
-			}
-			<AppBarTab>{Constants.manifest.version}</AppBarTab>
-		</ScrollView>
-	</View>
-);
+const AppBar = ({ user }) => {
+	if (!user) {
+		return (
+			<View horizontal style={styles.container}>
+				<Text style={styles.text}>Welcome to Kalles studio!</Text>
+				<Text style={styles.text}>{Constants.manifest.version}</Text>
+			</View>
+		);
+	}
+	return (
+		<View style={styles.container}>
+			<ScrollView horizontal>
+				<AppBarTab to="/map">Map</AppBarTab>
+				<AppBarTab to="/messages">Messages</AppBarTab>
+				<AppBarTab to="/newMessage">New message</AppBarTab>
+				<AppBarTab to="/userinfo">{user.username}</AppBarTab>
+				<AppBarTab>{Constants.manifest.version}</AppBarTab>
+			</ScrollView>
+		</View>
+	);
+};
 
 const AppBarTab = ({ to, children }) => (
 	<Link to={to}>
