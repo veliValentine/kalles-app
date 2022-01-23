a690593605a8b04c86ef1d8f247a0e64012d57e5
unknown <nikkevanukas1995@gmail.com>
2022-01-23 16:36:42 +0200

    feat: wake api server

--- client/App.js
+++ client/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Main from "./src/Main";
 
 import { NativeRouter } from "react-router-native";
+import instance from "./src/service/instance/apiInstance";
 
-const App = () => (
-	<NativeRouter>
-		<Main />
-	</NativeRouter>
-);
+const wakeApiServer = async () => {
+	const { data } = await instance.get("/health");
+	console.log(`Health data ${data}`);
+};
+
+const App = () => {
+	useEffect(wakeApiServer, []);
+	return (
+		<NativeRouter>
+			<Main />
+		</NativeRouter>
+	);
+};
 
 export default App;
